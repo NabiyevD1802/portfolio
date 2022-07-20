9e68547d66a73a811d78219e018b23666f3325ee
NabiyevD1802 <nabiyevdavlatbek1818@gmail.com>
2022-07-20 17:36:01 +0500

    first-commit

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+/**=============typing animation ================== */
+
+var typed = new Typed(".typing", {
+  strings: ["", "Web Designer", "Back-End Web Developer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
